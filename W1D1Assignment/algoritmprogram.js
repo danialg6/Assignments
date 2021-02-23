76e88e54a6c05089e0ca72224c6f9d547c60c48f
Danial5621 <78656217+Danial5621@users.noreply.github.com>
2021-02-22 18:22:19 -0600

    w1d1Assignment

---
+++ W1D1Assignment/algoritmprogram.js
@@ -0,0 +1,13 @@
+let time=prompt("please enter a time: ");
+if(time=="7"){
+    console.log("walk-up and stretch body for 5 minute");
+    }else if(time=="7:5"){
+    console.log("run for 10 minute");
+    }else if(time=="7:15"){
+    console.log("do 15 push up with in 3 minute");
+    }else if(time=="7:18"){
+    console.log("settle your body by taking fresh air");
+    }else {
+    console.log("you have no time take your break fast and start reading ");
+    }
+    
---
+++ W1D1Assignment/introDemoCode.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log(5+5); //this out will give us 10
+const x=5;
+const y=5;
+console.log(x+y);// this will gives us 10
+const x=6;
+const y=7;
+console.log(x+y);//this gives error
